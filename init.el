a06e206b65d21cb169120c3b1a1aa1f0e6641a99
Burin Choomnuan <agilecreativity@gmail.com>
2016-07-04 12:24:01 -0400

    Enable some appearance settings

--- init.el
+++ init.el
@@ -11,3 +11,274 @@
 
 (load-theme 'zenburn)
 (set-frame-font "Source Code Pro Bold 14" nil t)
+
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+(setq inhibit-startup-message t)
+
+(setq settings-dir
+      (expand-file-name "settings" user-emacs-directory))
+(add-to-list 'load-path settings-dir)
+
+;; Set up appearance early
+(require 'appearance)
+
+;; Settings for currently logged in user
+(setq user-settings-dir
+      (concat user-emacs-directory "users/" user-login-name))
+(add-to-list 'load-path user-settings-dir)
+
+; ;; Add external projects to load path
+; (dolist (project (directory-files site-lisp-dir t "\\w+"))
+;   (when (file-directory-p project)
+;     (add-to-list 'load-path project)))
+
+;; Save point position between sessions
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (expand-file-name ".places" user-emacs-directory))
+
+;; Are we on a mac?
+(setq is-mac (equal system-type 'darwin))
+
+;; Or are we on linux?
+(setq is-linux (equal system-type 'gnu/linux))
+
+; ;; Setup packages
+; (require 'setup-package)
+;
+; ;; Install extensions if they're missing
+; (defun init--install-packages ()
+;   (packages-install
+;    '(magit
+;      edn
+;      inflections
+;      hydra
+;      paredit
+;      move-text
+;      gist
+;      htmlize
+;      visual-regexp
+;      markdown-mode
+;      fill-column-indicator
+;      flycheck
+;      flycheck-pos-tip
+;      flycheck-clojure
+;      flx
+;      f
+;      flx-ido
+;      dired-details
+;      css-eldoc
+;      yasnippet
+;      smartparens
+;      ido-vertical-mode
+;      ido-at-point
+;      simple-httpd
+;      guide-key
+;      nodejs-repl
+;      restclient
+;      highlight-escape-sequences
+;      whitespace-cleanup-mode
+;      elisp-slime-nav
+;      dockerfile-mode
+;      clojure-mode
+;      clojure-mode-extra-font-locking
+;      groovy-mode
+;      prodigy
+;      cider
+;      yesql-ghosts
+;      string-edit
+;      ;; Some more packages that I need
+;      atom-dark-theme
+;      cyberpunk-theme
+;      molokai-theme
+;      seoul256-theme
+;      solarized-theme
+;      zenburn-theme
+;      ;; Misc packages
+;      ag
+;      github-browse-file
+;      git-timemachine
+;      ;; Ruby development
+;      bundler
+;      goto-gem
+;      ;helm-rails
+;      ;helm-rb
+;      inf-ruby
+;      projectile
+;      projectile-rails
+;      rbenv
+;      robe
+;      rspec-mode
+;      ruby-additional
+;      ruby-block
+;      ruby-hash-syntax
+;      ruby-refactor
+;      ruby-tools
+;      smartscan ;; Quickly jumps between symbols found at point
+;      yaml-mode
+;      yari
+;      ;; Misc packages
+;      command-log-mode ;; log the command for screencast/etc
+;      feature-mode
+;      helm-ag
+;      rainbow-mode
+;      rainbow-delimiters
+;      tagedit ;; paredit like for html
+;      )))
+;
+; (condition-case nil
+;     (init--install-packages)
+;   (error
+;    (package-refresh-contents)
+;    (init--install-packages)))
+;
+; ;; Lets start with a smattering of sanity
+; (require 'sane-defaults)
+;
+; ;; Setup environment variables from the user's shell.
+; (when is-mac
+;   (require-package 'exec-path-from-shell)
+;   (exec-path-from-shell-initialize))
+;
+; ;; guide-key
+; (require 'guide-key)
+; (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8" "C-x +"))
+; (guide-key-mode 1)
+; (setq guide-key/recursive-key-sequence-flag t)
+; (setq guide-key/popup-window-position 'bottom)
+;
+; ;; Setup extensions
+; (eval-after-load 'ido '(require 'setup-ido))
+; (eval-after-load 'org '(require 'setup-org))
+; (eval-after-load 'dired '(require 'setup-dired))
+; (eval-after-load 'magit '(require 'setup-magit))
+; (eval-after-load 'grep '(require 'setup-rgrep))
+; (eval-after-load 'shell '(require 'setup-shell))
+; (require 'setup-hippie)
+; (require 'setup-yasnippet)
+; (require 'setup-perspective)
+; (require 'setup-ffip)
+; (require 'setup-html-mode)
+; (require 'setup-paredit)
+;
+; (require 'prodigy)
+; (global-set-key (kbd "C-x M-m") 'prodigy)
+;
+; ;; Font lock dash.el
+; (eval-after-load "dash" '(dash-enable-font-lock))
+;
+; ;; Default setup of smartparens
+; (require 'smartparens-config)
+; (setq sp-autoescape-string-quote nil)
+; (--each '(css-mode-hook
+;           restclient-mode-hook
+;           js-mode-hook
+;           java-mode
+;           ruby-mode
+;           markdown-mode
+;           groovy-mode
+;           scala-mode)
+;   (add-hook it 'turn-on-smartparens-mode))
+;
+; ;; Language specific setup files
+; (eval-after-load 'js2-mode '(require 'setup-js2-mode))
+; (eval-after-load 'ruby-mode '(require 'setup-ruby-mode))
+; (eval-after-load 'clojure-mode '(require 'setup-clojure-mode))
+; (eval-after-load 'markdown-mode '(require 'setup-markdown-mode))
+;
+; ;; Load stuff on demand
+; (autoload 'skewer-start "setup-skewer" nil t)
+; (autoload 'skewer-demo "setup-skewer" nil t)
+; (autoload 'auto-complete-mode "auto-complete" nil t)
+; (eval-after-load 'flycheck '(require 'setup-flycheck))
+;
+; ;; Map files to modes
+; (require 'mode-mappings)
+;
+; ;; Highlight escape sequences
+; (require 'highlight-escape-sequences)
+; (hes-mode)
+; (put 'font-lock-regexp-grouping-backslash 'face-alias 'font-lock-builtin-face)
+;
+; ;; Visual regexp
+; (require 'visual-regexp)
+; (define-key global-map (kbd "M-&") 'vr/query-replace)
+; (define-key global-map (kbd "M-/") 'vr/replace)
+;
+; ;; Functions (load all files in defuns-dir)
+; (setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
+; (dolist (file (directory-files defuns-dir t "\\w+"))
+;   (when (file-regular-p file)
+;     (load file)))
+;
+; (require 'expand-region)
+; (require 'multiple-cursors)
+; (require 'delsel)
+; (require 'jump-char)
+; (require 'eproject)
+; (require 'wgrep)
+; (require 'smart-forward)
+; (require 'change-inner)
+; (require 'multifiles)
+;
+; ;; Don't use expand-region fast keys
+; (setq expand-region-fast-keys-enabled nil)
+;
+; ;; Show expand-region command used
+; (setq er--show-expansion-message t)
+;
+; ;; Fill column indicator
+; (require 'fill-column-indicator)
+; (setq fci-rule-color "#111122")
+;
+; ;; Browse kill ring
+; (require 'browse-kill-ring)
+; (setq browse-kill-ring-quit-action 'save-and-restore)
+;
+; ;; Smart M-x is smart
+; (require 'smex)
+; (smex-initialize)
+;
+; ;; Setup key bindings
+; (require 'key-bindings)
+;
+; ;; Misc
+; (require 'project-archetypes)
+; (require 'my-misc)
+; (when is-mac (require 'mac))
+;
+; ;; Elisp go-to-definition with M-. and back again with M-,
+; (autoload 'elisp-slime-nav-mode "elisp-slime-nav")
+; (add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t) (eldoc-mode 1)))
+;
+; ;; Emacs server
+; (require 'server)
+; (unless (server-running-p)
+;   (server-start))
+;
+; ;; Run at full power please
+; (put 'downcase-region 'disabled nil)
+; (put 'upcase-region 'disabled nil)
+; (put 'narrow-to-region 'disabled nil)
+;
+; ;; Conclude init by setting up specifics for the current user
+; (when (file-exists-p user-settings-dir)
+;   (mapc 'load (directory-files user-settings-dir nil "^[^#].*el$")))
+;
+; ;; Experimental {{
+; ;; NOTE: this will interfere with the flow of paredit so disable for now
+; ;; From: https://www.gnu.org/software/emacs/manual/html_node/efaq/Matching-parentheses.html
+; ;(global-set-key "%" 'match-paren)
+; ;(defun match-paren (arg)
+; ;  "Go to the matching paren if on a paren; otherwise insert %."
+; ;  (interactive "p")
+; ;  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+; ;        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+; ;        (t (self-insert-command (or arg 1)))))
+;
+; ;; Better default
+; (setq scroll-margin 5
+;       scroll-preserve-screen-position 1)
---
+++ users/bchoomnuan/init.el
@@ -0,0 +1,35 @@
+;; Default js indentation levels
+(setq-default js2-basic-offset 2)
+(setq js-indent-level 2)
+
+;; Disallow scrolling with mouse wheel
+; (when window-system
+;   (mouse-wheel-mode -1))
+
+;; Monday ftw
+(set-variable 'calendar-week-start-day 1)
+
+;; Font size
+(define-key global-map (kbd "C-+") 'zoom-in)
+;(define-key global-map (kbd "C--") 'zoom-out)
+
+;; rainbow-delimeters-mode settings
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;; starting directory
+;(setq default-directory (concat (getenv "HOME") "/codes/others"))
+
+;; Add sensible default settings
+; (sensible-defaults/open-files-from-home-directory)
+; (sensible-defaults/increase-gc-threshold)
+; (sensible-defaults/backup-to-temp-directory)
+; (sensible-defaults/delete-trailing-whitespace)
+; (sensible-defaults/treat-camelcase-as-separate-words)
+; (sensible-defaults/automatically-follow-symlinks)
+; (sensible-defaults/single-space-after-periods)
+; (sensible-defaults/offer-to-create-parent-directories-on-save)
+; (sensible-defaults/apply-changes-to-highlighted-region)
+; (sensible-defaults/overwrite-selected-text)
+
+;; Experimental for Org mode
+(setq org-cycle-emulate-tab 'white)
