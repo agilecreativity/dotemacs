1a3df15b875cba7995c4a65602db362b17fd958b
Burin Choomnuan <agilecreativity@gmail.com>
2016-12-11 16:10:40 -0500

    WIP

--- init.el
+++ init.el
@@ -181,7 +181,7 @@
 
 ;; Elisp go-to-definition with M-. and back again with M-,
 (autoload 'elisp-slime-nav-mode "elisp-slime-nav")
-(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t) (eldoc-mode 1)))
+(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t) (eldoc-mode t)))
 
 ;; Emacs server
 (require 'server)
