f1f0ba8aed11ea2c6635077de36b29ff2fe1f013
Burin Choomnuan <agilecreativity@gmail.com>
2017-02-10 20:35:11 -0500

    Add some config for org-babel use

--- custom.el
+++ custom.el
@@ -6,10 +6,10 @@
  '(custom-safe-themes
    (quote
     ("9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "14f0fbf6f7851bfa60bf1f30347003e2348bf7a1005570fd758133c87dafe08f" "486759384769d44b22bb46072726c2cfb3ccc3d49342e5af1854784d505ffc01" "4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" "70403e220d6d7100bae7775b3334eddeb340ba9c37f4b39c189c2c29d458543b" "a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "0e219d63550634bc5b0c214aced55eb9528640377daf486e13fb18a32bf39856" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "cdbd0a803de328a4986659d799659939d13ec01da1f482d838b68038c1bb35e8" "40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" default)))
- '(neo-window-position (quote right))
+ '(neo-window-position (quote left))
  '(package-selected-packages
    (quote
-    (org-journal rainbow-blocks ansible emms-info-mediainfo arduino-mode json-reformat aws-ec2 helm-aws darkokai-theme powershell package-build shut-up epl git commander f dash s))))
+    (elisp-slime-nav use-package org-journal rainbow-blocks ansible emms-info-mediainfo arduino-mode json-reformat aws-ec2 helm-aws darkokai-theme powershell package-build shut-up epl git commander f dash s))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- settings/setup-org.el
+++ settings/setup-org.el
@@ -18,4 +18,34 @@
 ;; For org-journal
 (setq org-journal-dir "~/Dropbox/org/journal")
 
+;; Experimental use of `use-package' to install and config
+(use-package org
+  :config
+  ;; Need to install `graphviz' locally first
+  (add-to-list 'org-src-lang-modes '("dot" . "graphviz-dot"))
+
+  ;; Add languages that we can use with org-babel
+  (org-babel-do-load-languages 'org-babel-load-languages
+                               '((sh . t)
+                                 (js . t)
+                                 (emacs-lisp . t)
+                                 (clojure . t)
+                                 (python . t)
+                                 (dot . t)
+                                 (ruby . t)
+                                 (css . t))))
+
+;; Use fancy list in org-mode
+(use-package org-bullets
+  :ensure t
+  :config
+  (progn
+    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))))
+
+(use-package ob-clojure
+  :init
+  (setq org-babel-clojure-backend 'cider))
+
+;; See: http://orgmode.org/cgit.cgi/org-mode.git/plain/lisp/ob-clojure.el
+
 (provide 'setup-org)
